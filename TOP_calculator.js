bb92747b8765ae4f070fdebf3c62c80e260be605
Igor-D-art <gorgsemenon1@gmail.com>
2021-11-10 18:45:28 +0200

    Another try

--- TOP_calculator.js
+++ TOP_calculator.js
@@ -1,31 +1,61 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll("button");
-let displayValue = [];
-let operands=[];
+let inputValue = []; // raw input 
+let inputString = ''; 
+let defaultOperand=0;
+let operand;
+let operator;
 let result; 
 
 let input = buttons.forEach(button => {
     button.addEventListener('click', ()=>{
-      digit = button.textContent;
-      displayValue.push(digit);
-      return displayValue + displayInput();
+      inputValue.push(button.textContent);
+      inputString = inputValue.join('');
+      console.log("Input value =" + inputValue);
+      console.log("Input string =" + inputString);
+      return inputString + parseInput();
     });
 });
 
-function displayInput(){
-    display.value=displayValue.join('');
-    console.log(display.value);
-    return display.value + operate();
+function parseInput(){
+   let numbers = /\d/g;
+   let operators = /\+|\-|\*|\/|\=/g; 
+   let firstOperIndex = inputString.indexOf(operators);
+
+   console.log(firstOperIndex);
+
+   number = inputString.match(numbers); 
+   if (number!==null){
+       operand = parseFloat(number.join(''));
+   } else {
+       operand=defaultOperand;
+   };
+
+   operator = inputString.match(operators);
+   if (operator!==null){
+       inputString='';
+   }
+    console.log(operator);
+    console.log("Default oprand= " + defaultOperand);
+    console.log("Operator =" + operator);
+    console.log("Operand =" + operand);
+    console.log("Input s tring after = ")+ inputString;
+
+   display.value=operand;
+   return operand + calculate();
 };
 
-function operate(){
-    if (display.value.includes('+')){
+function calculate(){
+    if (operator=='+'){
        addition();
     };
 };
 
 function addition(){
-    operands = display.value.split('+');
-   
-
+   result = defaultOperand+operand;
+   defaultOperand=operand; 
+   display.value = result;
+   operand=0;
+   inputString='';
+   console.log("Result = " + result + "\n ");
 }
